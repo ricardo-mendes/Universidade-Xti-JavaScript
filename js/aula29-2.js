fbf9588a08f17bf7752befa5a227b1fba6869429
Ricardo Mendes <rmendes06@hotmail.com>
2014-04-06 15:45:18 -0300

    Update aula29-2.js

--- js/aula29-2.js
+++ js/aula29-2.js
@@ -1,4 +1,3 @@
-
 /**** BENEFÍCIOS do DOM nivel 2  ****/
 
 // Ele é baseado todo em uma especificação, existe todo um trabalho de especificação desses manipuladores de eventos
@@ -8,13 +7,13 @@
 
 window.onload = function(){
     var btn = document.getElementById("botao");
-	btn.onclick = function(){return false}; // não vai pra aula 12 , que é um atributo que eu coloquei no elemento html hrfe="aula12.js"; , quebrei o manipulador de evento padrão do browse
-	//btn.onclick = function(){um(); dois();}	// btn.onclick = function(){um(); dois();} // vai até funcionar , mas imagina se tivesse trabalhando com um script grande , dependendo da ação de um usuário eu adiciono mais um evento para ser manipulado e dependendo da ação eu aciciono outro evento para ser manipulado, ai eu não teria essa possibilidade
-	//btn.onclick = function(){dois();}
-	//só vai chamar o 2
+    btn.onclick = function(){return false}; // não vai pra aula 12 , que é um atributo que eu coloquei no elemento html hrfe="aula12.js"; , quebrei o manipulador de evento padrão do browse
+    //btn.onclick = function(){um(); dois();}	// btn.onclick = function(){um(); dois();} // vai até funcionar , mas imagina se tivesse trabalhando com um script grande , dependendo da ação de um usuário eu adiciono mais um evento para ser manipulado e dependendo da ação eu aciciono outro evento para ser manipulado, ai eu não teria essa possibilidade
+    //btn.onclick = function(){dois();}
+    //só vai chamar o 2
 	
-	//btn.addEventListener("click", um, false);
-	//btn.addEventListener("click", dois, false);  // atrelhou os dois manipuladores de evento pra um mesmo evento neste elemento
+    //btn.addEventListener("click", um, false);
+    //btn.addEventListener("click", dois, false);  // atrelhou os dois manipuladores de evento pra um mesmo evento neste elemento
 }
 
 function um(){
